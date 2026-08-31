93acf266dc8c6582df63c2761c9090f3f0e4415b
047Gang <marikgg2@gmail.com>
2026-08-31 12:26:36 +0200

    Update Tool.java

--- src/main/java/com/construction/model/Tool.java
+++ src/main/java/com/construction/model/Tool.java
@@ -1,13 +1,20 @@
 package com.construction.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-
-@Data
-@AllArgsConstructor
 public class Tool {
     private String id;
     private String name;
-    private String description;
-    private String safetyTips;
+    private String category;
+    private String safetyRules;
+
+    public Tool(String id, String name, String category, String safetyRules) {
+        this.id = id;
+        this.name = name;
+        this.category = category;
+        this.safetyRules = safetyRules;
+    }
+
+    public String getId() { return id; }
+    public String getName() { return name; }
+    public String getCategory() { return category; }
+    public String getSafetyRules() { return safetyRules; }
 }
